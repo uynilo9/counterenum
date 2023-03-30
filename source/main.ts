72a0ec6269742d3841a94eecdcb82ac7ddb8f49a
uynilo9 <uynilo9@outlook.com>
2023-03-30 20:01:22 +0800

    COMMIT: Finish first test

--- source/main.ts
+++ source/main.ts
@@ -1,4 +1,6 @@
-import { CounterEnum } from './types';
+export type CounterEnum<T extends Record<string, string>> = {
+    [K in T[keyof T]]: { [P in keyof T]: T[P] extends K ? P : never }[keyof T];
+};
 export default function counterenum<T extends Record<string, string>>(e: T): CounterEnum<T> {
     return Object.entries(e).reduce(function(accumulator, [key, value]) {
         accumulator[value as keyof CounterEnum<T>] = key as keyof T;
--- source/types.ts
+++
@@ -1,3 +0,0 @@
-export type CounterEnum<T extends Record<string, string>> = {
-    [K in T[keyof T]]: { [P in keyof T]: T[P] extends K ? P : never }[keyof T];
-};
